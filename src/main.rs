ba6f2738f4e4655ed8b593c7ef22c66a336b8e64
Sven <isvenshi@gmail.com>
2025-04-14 01:57:44 +0800

    :sparkles: dns服务运行

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use hickory_server::authority::MessageResponseBuilder;
+use hickory_server::server::{Request, ResponseHandler, ResponseInfo};
+use hickory_server::{server::RequestHandler, ServerFuture};
+use tokio::net::UdpSocket;
+
+pub struct RustDnsRequestHandler {}
+
+#[async_trait::async_trait]
+impl RequestHandler for RustDnsRequestHandler {
+    async fn handle_request<R: ResponseHandler>(
+        &self,
+        request: &Request,
+        mut response_handle: R,
+    ) -> ResponseInfo {
+
+        println!("Handling request: {:?}", request);
+        let response = MessageResponseBuilder::from_message_request(request).build_no_records(*request.header());
+
+        response_handle.send_response(response).await.unwrap()
+    }
+
+}
+#[tokio::main]
+async fn main() {
+    let handler = RustDnsRequestHandler {};
+    let mut future = ServerFuture::new(handler);
+    let bind1 = UdpSocket::bind("0.0.0.0:253");
+    future.register_socket(bind1.await.unwrap());
+    future.block_until_done().await.unwrap();
 }
