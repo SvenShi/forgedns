c0e1c503dca1feed13cb147d9a62d9fa898b9c98
Sven <isvenshi@gmail.com>
2025-10-14 15:03:07 +0800

    :page_facing_up: change file license

--- src/pkg/upstream/mod.rs
+++ src/pkg/upstream/mod.rs
@@ -1,15 +1,5 @@
-/*
- * Copyright 2025 Sven Shi
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// SPDX-FileCopyrightText: 2025 Sven Shi
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 use crate::core::context::DnsContext;
 use crate::pkg::upstream::pool::pipeline::PipelinePool;
@@ -18,8 +8,8 @@ use crate::pkg::upstream::pool::tcp_conn::{TcpConnection, TcpConnectionBuilder};
 use crate::pkg::upstream::pool::udp_conn::{UdpConnection, UdpConnectionBuilder};
 use crate::pkg::upstream::pool::{Connection, ConnectionPool};
 use async_trait::async_trait;
-use hickory_proto::xfer::DnsResponse;
 use hickory_proto::ProtoError;
+use hickory_proto::xfer::DnsResponse;
 use serde::Deserialize;
 use std::net::IpAddr;
 use std::str::FromStr;
--- src/pkg/upstream/pool/reuse.rs
+++ src/pkg/upstream/pool/reuse.rs
@@ -1,27 +1,18 @@
-/*
- * Copyright 2025 Sven Shi
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// SPDX-FileCopyrightText: 2025 Sven Shi
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use crate::core::app_clock::AppClock;
 use crate::pkg::upstream::pool::{
-    close_conns, start_maintenance, Connection, ConnectionBuilder, ConnectionPool,
+    Connection, ConnectionBuilder, ConnectionPool, close_conns, start_maintenance,
 };
 use async_trait::async_trait;
 use crossbeam_queue::ArrayQueue;
+use hickory_proto::ProtoError;
 use hickory_proto::op::Message;
 use hickory_proto::xfer::DnsResponse;
-use hickory_proto::ProtoError;
 use std::fmt::Debug;
-use std::sync::atomic::{AtomicU16, AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::sync::atomic::{AtomicU16, AtomicUsize, Ordering};
 use std::time::Duration;
 use tokio::sync::Notify;
 use tracing::{debug, info, warn};
