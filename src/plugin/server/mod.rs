7a9eb598bdd0c69f729ff32c55a6493252bab01b
Sven <isvenshi@gmail.com>
2025-10-14 17:33:16 +0800

    :page_facing_up: license format

--- src/plugin/server/mod.rs
+++ src/plugin/server/mod.rs
@@ -1,3 +1,5 @@
-// SPDX-FileCopyrightText: 2025 Sven Shi
-// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * SPDX-FileCopyrightText: 2025 Sven Shi
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 pub mod udp;
